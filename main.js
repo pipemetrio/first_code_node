2cd5f5c121ba9332d828c1d466c3b08058e68f2c
pipemetrio <elgraffitero@gmail.com>
2025-10-17 08:21:50 -0500

    modulo os

--- main.js
+++ main.js
@@ -1,1 +1,57 @@
-console.log("hola mundo");
+import os, { setPriority } from "os"
+function modulo_os() {
+    // for (let i= 0; i<= 100; i++){
+    //     if (i%3 ==0){
+    //         console.log(i);
+    //     }
+    // }
+    //   console.log(os);
+    //freemem muestra la memoria libre
+    // const memory = (os.freemem / 1024) 
+    //   console.log(memory / 1024);
+
+    // arch muestra la arquitectura del sistema
+    // console.log(arch());
+
+    // cpus muestra el tipo de procesador del equipo
+    // console.log(os.cpus());
+
+    // getpriority muestra la prioridad del OS con el que arranca el equipo
+    // console.log(os.getPriority())
+
+    //hostname muestra el nombre del equipo 
+    // console.log(os.hostname()
+
+    //loadavg muestra programas ejecutables en segundo plano de alta carga
+    // console.log(os.loadavg());
+
+    //platform muestra la plataform en la que se encuentra el OS
+    // console.log(os.platform());
+
+    //release muestra la version especifica del OS
+    // console.log(os.release());
+
+    // setPriority prioridad del sistema o procesos
+    // console.log(setPriority(10));
+
+    //tmpdir muestra la ruta de la carpeta de archivos temporales
+    // console.log(os.tmpdir());
+
+    // totalmem muestra la capacidad de la/s tarejeta/s RAM
+    // console.log(os.totalmem());
+
+    //type muestra el nombre del sistema operativo
+    // console.log(os.type());
+
+    // userInfo muestra la informacion del usuario o usuarios del equipo
+    // console.log(os.userInfo());
+
+    //uptime muestra el tiempo que el OS esta en funcionamiento sin interrupciones
+    // console.log(os.uptime());
+
+    //version muestra la version del OS
+    // console.log(os.version());
+
+    // machine muestra el tipo de hardware subyacente de la maquina
+    // console.log(os.machine());
+}
